f54631b60eed2a27dd9a2db2393199950c0199e8
Lima Junior <fjdlimajunior@gmail.com>
2024-04-19 10:24:38 -0300

    update folder 6

--- 6-exercicios/ex1.js
+++ 6-exercicios/ex1.js
@@ -5,16 +5,17 @@ se media >= 5 e <7 imprima "recuperação"
 se media >= 7 "aprovado"
 */
 
-const {gets, print} = require('./funcoesAuxiliares-ex1');
+const { gets, print } = require('./funcoesAuxiliares-ex1');
 
-const media = gets();
+const media = gets(); //busca a informação contida no arquivo ./funcoesAuxiliares-ex1.js
+
 
 if (media >= 0 && media < 5) {
     print(media + ' - Reprovado!');
-}else if (media >= 5 && media < 7) {
+} else if (media >= 5 && media < 7) {
     print(media + ' - Recuperação!');
-} else if(media >= 7 && media <= 10) {
+} else if (media >= 7 && media <= 10) {
     print(media + ' - Aprovado!');
-} else if(media > 10) {
+} else if (media > 10) {
     print('Dados incorretos!');
 }
