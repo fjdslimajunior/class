ce38bd6924c836368742e1cc66abe6c7dfa9a648
Lima Junior <fjdlimajunior@gmail.com>
2024-04-21 17:14:06 -0300

    update more include ternary mod

--- !motiro/test.js
+++ !motiro/test.js
@@ -39,8 +39,21 @@
 // const multiConta = contaSemente(5,15);
 // console.log(multiConta);
 
-function listaDeEstudante(est1 = 'Lucas', est2 = 'Cainã', est3 = 'Daniel') {
-    console.log(`As pessoas que se destacaram foram: ${est1}, ${est2} e ${est3}.`);
-}
+// function listaDeEstudante(est1 = 'Lucas', est2 = 'Cainã', est3 = 'Daniel') {
+//     console.log(`As pessoas que se destacaram foram: ${est1}, ${est2} e ${est3}.`);
+// }
 
-listaDeEstudante('Laura', 'Jamile', 'Maiara');
+// listaDeEstudante('Laura', 'Jamile', 'Maiara');
+const mLargura = 100;
+const mComprimento = 200;
+
+/*transformando em ternário exemplo: condicao ? expressao1 : expressao2;
+Isso significa que se a condicao for verdadeira, a expressao1 será avaliada e retornada, e se a condicao for falsa, a expressao2 será avaliada e retornada.
+
+No nosso exemplo, a condicao é largura <= MAX_WIDTH && comprimento <= MAX_LENGTH. Se essa condição for verdadeira, a expressao1 será 'A mesa cabe na sala!', 
+e se a condição for falsa, a expressao2 será 'A mesa é maior que o espaço disponível!'.
+*/
+const areaDaMesa = (largura, comprimento) => largura <= mLargura && comprimento <= mComprimento ? 'A mesa cabe na sala!' :
+        'A mesa é maior que o espaço disponível!';
+
+console.log(areaDaMesa(300,350));
