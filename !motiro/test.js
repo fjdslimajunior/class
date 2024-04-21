731d4bb55e1732b24400b5123b37caf7f305b2d2
Lima Junior <fjdlimajunior@gmail.com>
2024-04-21 03:10:46 -0300

    teste create function motiro

--- !motiro/test.js
+++ !motiro/test.js
@@ -7,15 +7,27 @@
 //     console.log(`Boas-vindas, ${name}.`);
 // }    
 
-// saudacao("Xigo");
+// // saudacao("Xigo");
 
-let termo = 'foo';
+// let termo = 'foo';
 
-//console.log(termo.includes("f"));
+// //console.log(termo.includes("f"));
 
-if (termo.includes('f')) {
-    console.log('Valor com a chave inclusa');
-} else {
-    console.log('Valor sem a chave inclusa');
+// if (termo.includes('f')) {
+//     console.log('Valor com a chave inclusa');
+// } else {
+//     console.log('Valor sem a chave inclusa');
    
+// }
+
+//criação de função e teste
+function constaSemente(linhas,colunas) {
+    return linhas * colunas;
 }
+function custoSemente(linhas,colunas) {
+    return constaSemente(linhas,colunas) * 0.80;
+}
+//const quantSemente = constaSemente(11,23);
+const custoTotal = custoSemente(11,23);
+
+console.log(custoTotal);
